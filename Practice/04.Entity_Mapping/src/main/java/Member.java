3fc6a2c2e8275d095fba382b780f6076a447b737
김태민 <kw3135@gmail.com>
2022-08-18 08:18:36 +0900

    JPA 프로그래밍_4장 4-3 정리

---
+++ Practice/04.Entity_Mapping/src/main/java/Member.java
@@ -0,0 +1,34 @@
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "MEMBER")
+public class Member
+{
+    @Id
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name = "NAME")
+    private String username;
+
+    //매핑 정보가 없는 필드
+    private Integer age;
+
+    //추가
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType; //구분
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate; //생성일
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate; //수정일
+
+    @Lob
+    private String description; //회원 설명
+}
+
