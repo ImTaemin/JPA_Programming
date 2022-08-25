fdbf7e723d4943405f58173b93ca05ff4bf895b3
김태민 <kw3135@gmail.com>
2022-08-25 09:17:18 +0900

    JPA 프로그래밍_9장 실전예제

--- Practice/Example/src/main/java/entity/Delivery.java
+++ Practice/Example/src/main/java/entity/Delivery.java
@@ -1,8 +1,12 @@
 package entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter @Setter
 public class Delivery
 {
     @Id @GeneratedValue
@@ -12,70 +16,9 @@ public class Delivery
     @OneToOne(mappedBy = "delivery")
     private Order order;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
 
     @Enumerated(EnumType.STRING)
     private DeliveryStatus status;
-
-    public Long getId()
-    {
-        return id;
-    }
-
-    public void setId(Long id)
-    {
-        this.id = id;
-    }
-
-    public Order getOrder()
-    {
-        return order;
-    }
-
-    public void setOrder(Order order)
-    {
-        this.order = order;
-    }
-
-    public String getCity()
-    {
-        return city;
-    }
-
-    public void setCity(String city)
-    {
-        this.city = city;
-    }
-
-    public String getStreet()
-    {
-        return street;
-    }
-
-    public void setStreet(String street)
-    {
-        this.street = street;
-    }
-
-    public String getZipcode()
-    {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode)
-    {
-        this.zipcode = zipcode;
-    }
-
-    public DeliveryStatus getStatus()
-    {
-        return status;
-    }
-
-    public void setStatus(DeliveryStatus status)
-    {
-        this.status = status;
-    }
 }
--- Practice/Example/src/main/java/entity/Member.java
+++ Practice/Example/src/main/java/entity/Member.java
@@ -1,12 +1,15 @@
 package entity;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter @Setter
 public class Member extends BaseEntity
 {
     @Id
@@ -16,70 +19,9 @@ public class Member extends BaseEntity
 
     private String name;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
 
     @OneToMany(mappedBy = "member")
-    private List<Order> orders = new ArrayList<Order>();
-
-    public Long getId()
-    {
-        return id;
-    }
-
-    public void setId(Long id)
-    {
-        this.id = id;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public String getCity()
-    {
-        return city;
-    }
-
-    public void setCity(String city)
-    {
-        this.city = city;
-    }
-
-    public String getStreet()
-    {
-        return street;
-    }
-
-    public void setStreet(String street)
-    {
-        this.street = street;
-    }
-
-    public String getZipcode()
-    {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode)
-    {
-        this.zipcode = zipcode;
-    }
-
-    public List<Order> getOrders()
-    {
-        return orders;
-    }
-
-    public void setOrders(List<Order> orders)
-    {
-        this.orders = orders;
-    }
+    private List<Order> orders = new ArrayList<>();
 }
